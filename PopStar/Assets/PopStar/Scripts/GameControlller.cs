dc5bb3eb3426c3140d892009cfb299440104c4fd
XiaoHang1006 <jianll1006@163.com>
2016-03-14 21:11:06 +0800

    1.项目创建  2.算法创建

---
+++ PopStar/Assets/PopStar/Scripts/GameControlller.cs
@@ -0,0 +1,193 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class GameControlller : MonoBehaviour
+{
+    public static GameControlller Ins;
+    public GameObject star;
+    public Transform StarParent;
+    public ArrayList starList;
+    public ArrayList SameColorStarList;
+    public ArrayList checkedStars;
+    private int rowNum = 9; //行号
+    private int colNum = 10; //列号
+    private Star currentStar;
+
+    public void Awake()
+    {
+        starList = new ArrayList();
+        checkedStars = new ArrayList();
+        SameColorStarList = new ArrayList();
+        Ins = this;
+    }
+
+    public void Start()
+    {
+        for (int rowIndex = 0; rowIndex < rowNum; rowIndex++)
+        {
+            ArrayList temp = new ArrayList();
+            for (int columIndex = 0; columIndex < colNum; columIndex++)
+            {
+                GameObject go = AddStar(rowIndex, columIndex);
+                temp.Add(go.GetComponent<Star>());
+            }
+            starList.Add(temp);
+        }
+    }
+
+    public GameObject AddStar(int row, int col)
+    {
+        GameObject go = Instantiate(star) as GameObject;
+        go.transform.parent = StarParent;
+        go.transform.localScale = Vector3.one;
+        go.GetComponent<Star>().UpdatePosition(row, col);
+        go.GetComponent<Star>().RandomCreateStarType();
+        return go;
+    }
+
+    //被点击
+    public void Select(Star star)
+    {
+        currentStar = star;
+        SameColorStarList.Add(star);
+        CheckAllSide(currentStar);
+        if (star.IsSelect)
+        {
+            if (SameColorStarList.Count > 1)
+            {
+                for (int i = 0; i < rowNum; i++)
+                {
+                    for (int j = 0; j < colNum; j++)
+                    {
+                        Star s = ((ArrayList)starList[i])[j] as Star;
+                        if (SameColorStarList.Contains(s))
+                        {
+                            ((ArrayList) starList[i])[j] = null;
+                            Destroy(s.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (SameColorStarList.Count > 1)
+            {
+                for (int i = 0; i < rowNum; i++)
+                {
+                    for (int j = 0; j < colNum; j++)
+                    {
+                        Star s = ((ArrayList)starList[i])[j] as Star;
+                        if (s != null)
+                        {
+                            s.IsSelect = false;
+                            if (SameColorStarList.Contains(s))
+                            {
+                                s.IsSelect = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        SameColorStarList.Clear();
+        checkedStars.Clear();
+    }
+
+    //检测上方
+    public void CheckUp(Star star)
+    {
+        int column = star.ColIndex;
+        int row = star.RowIndex;
+        if (column > 8)
+        {
+            return;
+        }
+        Star starUpSide = (Star)((ArrayList)starList[row])[column + 1];
+        if (starUpSide == null || starUpSide.starColor != star.starColor)
+        {
+            return;
+        }
+        SameColorStarList.Add(starUpSide);
+        CheckAllSide(starUpSide);
+    }
+
+    //检测下方
+    public void CheckDown(Star star)
+    {
+        int column = star.ColIndex;
+        int row = star.RowIndex;
+        if (column < 1)
+        {
+            return;
+        }
+        Star starDownSide = ((ArrayList)starList[row])[column - 1] as Star;
+        if (starDownSide == null || starDownSide.starColor != star.starColor)
+        {
+            return;
+        }
+        SameColorStarList.Add(starDownSide);
+        CheckAllSide(starDownSide);
+    }
+
+    //检测右方
+    public void CheckRight(Star star)
+    {
+        int column = star.ColIndex;
+        int row = star.RowIndex;
+        if (row > 7)
+        {
+            return;
+        }
+        Star starRightSide = ((ArrayList)starList[row + 1])[column] as Star;
+        if (starRightSide == null || starRightSide.starColor != star.starColor)
+        {
+            return;
+        }
+        SameColorStarList.Add(starRightSide);
+        CheckAllSide(starRightSide);
+    }
+
+    //检测左方
+    public void CheckLeft(Star star)
+    {
+        int column = star.ColIndex;
+        int row = star.RowIndex;
+        if (row < 1)
+        {
+            return;
+        }
+        Star starLeftSide = ((ArrayList)starList[row - 1])[column] as Star;
+        if (starLeftSide == null || starLeftSide.starColor != star.starColor)
+        {
+            return;
+        }
+        SameColorStarList.Add(starLeftSide);
+
+        CheckAllSide(starLeftSide);
+    }
+
+    //递归，不停检测四面
+    public void CheckAllSide(Star star)
+    {
+        if (IsChecked(star))
+        {
+            return;
+        }
+
+        checkedStars.Add(star);
+
+        CheckUp(star);
+        CheckDown(star);
+        CheckLeft(star);
+        CheckRight(star);
+    }
+
+    //是否已经检测
+    public bool IsChecked(Star star)
+    {
+        return checkedStars.Contains(star);
+    }
+
+}
---
+++ PopStar/Assets/PopStar/Scripts/Star.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public enum StarColor
+{
+    None,
+    Blue,
+    Green,
+    Purple,
+    Red,
+    Yellow,
+}
+
+public class Star : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject[] starType;
+    public int RowIndex; //行号
+    public int ColIndex; //列号
+    public StarColor starColor;  //存储颜色
+    private Image StarBG;
+    private GameObject StarBackground;
+    public bool isSelect;
+    public bool IsSelect
+    {
+        set
+        {
+            isSelect = value;
+            StarBackground.GetComponent<Star>().isSelect = isSelect;
+            if (isSelect)
+            {
+                StarBG.color = Color.red;
+            }
+            else
+            {
+                StarBG.color = Color.white;
+            }
+        }
+        get { return isSelect; }
+    }
+
+    public void Start()
+    {
+        StarBG = GetComponentInChildren<Image>();
+    }
+
+    public void UpdatePosition(int row, int col)
+    {
+        RowIndex = row;
+        ColIndex = col;
+        transform.localPosition = new Vector3(RowIndex * 60, ColIndex * 60, 0);
+    }
+
+    public void RandomCreateStarType()
+    {
+        if (StarBG != null)
+            return;
+        int starTypeIndex = Random.Range(0, starType.Length);
+        StarBackground = Instantiate(starType[starTypeIndex]) as GameObject;
+        StarBackground.transform.parent = this.transform;
+        StarBackground.transform.localScale = Vector3.one;
+        StarBackground.transform.localPosition = Vector3.zero;
+        string color = StarBackground.tag;
+        starColor = (StarColor)Enum.Parse(typeof(StarColor), color);
+        StarBackground.GetComponent<Star>().ColIndex = ColIndex;
+        StarBackground.GetComponent<Star>().RowIndex = RowIndex;
+        StarBackground.GetComponent<Star>().starColor = starColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameControlller.Ins.Select(this);
+    }
+}
